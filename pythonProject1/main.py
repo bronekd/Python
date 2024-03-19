4a04d1b128dfc5fc2f8f4c7061b835120e2a9642
PY-TU-01-24 <bronislavdrimal@email.cz>
2024-03-19 19:54:40 +0100

    Create program spine

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,2 +1,30 @@
 print("Hello world")
-# příkaz git pull stáhnu doma na PC 
+# příkaz git pull stáhnu doma na PC
+
+
+def print_menu():
+    pass
+
+def search():
+    pass
+
+def add_item():
+    pass
+
+def print_item():
+    pass
+
+def delete_item():
+    pass
+
+def replace_item():
+    pass
+
+def get_input():
+    pass
+
+def run():
+    pass
+
+
+run()
